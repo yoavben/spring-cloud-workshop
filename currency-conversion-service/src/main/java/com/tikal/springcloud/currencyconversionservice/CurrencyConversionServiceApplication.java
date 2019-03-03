c8d9599af3bbc9d79161fcd9e6264697891282ad
yoavsetoo <yoav@setoo.com>
2019-03-03 18:39:40 +0200

    step18 - end

---
+++ currency-conversion-service/src/main/java/com/tikal/springcloud/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -0,0 +1,14 @@
+package com.tikal.springcloud.currencyconversionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConversionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+	}
+
+}
+
