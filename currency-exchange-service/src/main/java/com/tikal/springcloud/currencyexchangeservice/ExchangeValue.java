336afbb19561921adbc8b67ed39fdd0e306d87db
yoavsetoo <yoav@setoo.com>
2019-03-03 18:26:03 +0200

    step16 end

--- currency-exchange-service/src/main/java/com/tikal/springcloud/currencyexchangeservice/ExchangeValue.java
+++ currency-exchange-service/src/main/java/com/tikal/springcloud/currencyexchangeservice/ExchangeValue.java
@@ -1,10 +1,18 @@
 package com.tikal.springcloud.currencyexchangeservice;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
+@Entity
 public class ExchangeValue {
+
+    @Id
     private Long id;
+    @Column(name = "currency_from")
     private String from;
+    @Column(name = "currency_to")
     private String to;
     private BigDecimal conversionMultiple;
     private int port;
---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into exchange_value (id,currency_from, currency_to, conversion_multiple,port) VALUES ( 1, 'USD', 'NIS', 3.65 ,0);
+insert into exchange_value (id,currency_from, currency_to, conversion_multiple,port) VALUES ( 2, 'NIS', 'USD', 0.27 ,0);
