1b8269f50c6020ca29f3976cbaab1059d1807cee
yoavsetoo <yoav@setoo.com>
2019-03-05 09:54:28 +0200

    step22-end

--- currency-conversion-service/src/main/java/com/tikal/springcloud/currencyconversionservice/CurrencyExchangeClientProxy.java
+++ currency-conversion-service/src/main/java/com/tikal/springcloud/currencyconversionservice/CurrencyExchangeClientProxy.java
@@ -1,10 +1,12 @@
 package com.tikal.springcloud.currencyconversionservice;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange-service",url = "http://localhost:8000")
+@FeignClient(name = "currency-exchange-service")
+@RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeClientProxy {
     @GetMapping("/currency-exchange/{from}/to/{to}")
     ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to);
